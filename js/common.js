277697b1dae16999865169e7920a2e80a8524998
Hyuckjin <43224608+hjCode1@users.noreply.github.com>
2019-04-15 09:01:46 +0900

    fix

--- js/common.js
+++ js/common.js
@@ -1,56 +1,58 @@
-$(document).ready(function(){
-    //메뉴 버튼
-    $('#menu_ico').click(function(){
+$(document).ready(function () {
+    // 메뉴 버튼
+    $('#menu_ico').click(function () {
         $(this).toggleClass('open');
 
-        setTimeout(function() {
+        setTimeout(function () {
             $('#side_nav_bg').toggleClass('on');
         }, 300);
 
         if ($('#side_nav_bg').hasClass('on')) {
             $('#side_nav').removeClass('on');
         } else {
-            setTimeout(function() {
+            setTimeout(function () {
                 $('#side_nav').addClass('on');
             }, 500);
         }
     });
 
-    //헤더 스크롤 show / hide
-    function headerShow(){
+    // 헤더 스크롤 show / hide
+    function headerShow() {
         let lastScroll = 0;
-        $(window).scroll(function(){
+        $(window).scroll(function () {
             const st = $(this).scrollTop();
-            if( st > lastScroll ){
+            if (st > lastScroll) {
                 $('header').addClass('on');
             }
-            else{
+            else {
                 $('header').removeClass('on');
             }
             lastScroll = st;
         });
     };
     headerShow();
-    //work move
-    function moveObj(){
+
+    // move obj
+    const obj = $('.layer');
+    function moveObj() {
         const move = 3;
         const rotate = 3;
 
-        $('.layer').each(function() {
-            $(this).mousemove(function(e) {
+        obj.each(function () {
+            $(this).mousemove(function (e) {
                 const docX = $(this).width();
                 const docY = $(this).height();
                 let moveX = (e.offsetX - docX / 2) / (docX / 2) * - move;
                 let moveY = (e.offsetY - docY / 2) / (docY / 2) * - move;
                 let rotateY = (e.offsetX / docX * rotate * 2) - rotate;
                 let rotateX = -(e.offsetY / docY * rotate * 2) - rotate;
-                
+
                 $(this).children('a').css({
                     'left': moveX + 'px',
                     'top': moveY + 'px',
                     'transform': 'rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg)'
                 });
-                
+
                 $(this).children('.work_title').css({
                     'right': moveX + 'px',
                     'bottom': moveY + 'px',
@@ -59,19 +61,37 @@ $(document).ready(function(){
             });
         });
     }
-    $('.layer').mouseenter(function(){
+    obj.mouseenter(function () {
         moveObj();
         $(this).children('a').css({
             'willChange': 'transform, left, top'
         });
     });
-    $('.layer').mouseleave(function () {
+    obj.mouseleave(function () {
         $(this).children('a').css({
             'left': 0,
             'top': 0,
             'transform': 'rotateX(0) rotateY(0)',
             'willChange': 'auto'
         });
-     });
+    });
 
-});
+    //scroll btn
+    const btnScroll = $('.scroll_up');
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 100 )
+            btnScroll.fadeIn();
+        else
+            btnScroll.fadeOut();
+    });
+    btnScroll.fadeOut();
+    btnScroll.click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+
+
+}); // end
